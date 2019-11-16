67957f7ffc6c3035b40240324518d1a1f4d5a12c
lpop <laur.pop.91@gmail.com>
2019-11-16 11:47:05 +0100

    My first commit

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+
+    public static AppiumDriver mobileDriver;
+
+    @BeforeTest
+    public void beforeTest() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "9");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "Appium");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
+        capabilities.setCapability("newCommandTimeout", 2000);
+        mobileDriver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+
+    @AfterTest
+    public void afterTest() {
+        mobileDriver.quit();
+    }
+
+
+    @Test
+    public void launchBrowser() {
+        mobileDriver.get("https://demo.midtrans.com/");
+        Assert.assertEquals(mobileDriver.getCurrentUrl(), "https://demo.midtrans.com/", "URL Mismatch");
+    }
+}
