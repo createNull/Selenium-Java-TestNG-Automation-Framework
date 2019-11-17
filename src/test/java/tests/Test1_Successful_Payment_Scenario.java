7af9a4551c2ac4339fc73c055e583b06f7743ec6
lpop <laur.pop.91@gmail.com>
2019-11-17 13:03:14 +0100

    Created 2 payment scenario test files

---
+++ src/test/java/tests/Test1_Successful_Payment_Scenario.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class Test1_Successful_Payment_Scenario extends BaseTest {
+
+    @Test
+    public void test01() {
+
+    }
+}
---
+++ src/test/java/tests/Test2_Failed_Payment_Scenario.java
@@ -0,0 +1,5 @@
+package tests;
+
+public class Test2_Failed_Payment_Scenario extends BaseTest {
+
+}
