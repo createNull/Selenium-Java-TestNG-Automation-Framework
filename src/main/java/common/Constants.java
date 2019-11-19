f55046fba5cc9ba609b8fc8dbb3ab33dac3ef342
lpop <laur.pop.91@gmail.com>
2019-11-19 11:00:29 +0100

    removed csv files and created new constants in Constants.java

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -1,10 +1,34 @@
 package common;
 
+
 public class Constants {
 
     public static final String URL = "https://demo.midtrans.com/";
 
-    public static final String BASE_DIR = "$DATADIR";
+    public static final String amount = "12000";
 
-    public static final String BASE_DIR_RAW = "src/test/java/resources/";
+    public static final String username = "TestName";
+
+    public static final String email = "test@example.com";
+
+    public static final String phoneNumber = "+46111122223333";
+
+    public static final String city = "Stockholm";
+
+    public static final String address = "Gesundaplan 38, Bromma";
+
+    public static final String postalCode = "16743";
+
+    public static final String itemName = "Midtrans Pillow";
+
+    public static final String cardNumberSuccess = "4811 1111 1111 1114";
+
+    public static final String cardNumberFail = "4911 1111 1111 1113";
+
+    public static final String expiryDate = "0220";
+
+    public static final String cvv = "123";
+
+    public static final String password = "112233";
+
 }
