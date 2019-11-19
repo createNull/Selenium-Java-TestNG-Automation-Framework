d7aff88ba58d7373ecae5e78e0f380e85a44d8d0
lpop <laur.pop.91@gmail.com>
2019-11-19 11:07:12 +0100

    refactored test files

--- src/test/java/tests/Test1_Successful_Payment_Scenario.java
+++ src/test/java/tests/Test1_Successful_Payment_Scenario.java
@@ -1,45 +1,45 @@
 package tests;
 
-import common.Constants;
 import org.testng.annotations.Test;
 import pages.*;
 
+import static common.Constants.*;
+
+
 public class Test1_Successful_Payment_Scenario extends BaseTest {
 
-    private LandingPage LandingSteps = new LandingPage(mobileDriver);
-    private OrderSummaryPage OrderSummarySteps = new OrderSummaryPage(mobileDriver);
-    private SelectPaymentPage SelectPaymentSteps = new SelectPaymentPage(mobileDriver);
-    private CardDetailsPage CardDetailsSteps = new CardDetailsPage(mobileDriver);
-    private BankTransactionPage BankTransactionSteps = new BankTransactionPage(mobileDriver);
-    private TransactionStatusPage TransactionStatusSteps = new TransactionStatusPage(mobileDriver);
-
-    private String amount, username, email, phoneNumber, city, address, postalCode, itemName,
-            cardNumberSuccess, expiryDate, cvv, password;
-
-    @Test(dataProvider = Constants.BASE_DIR + "Test1_Successful_Payment_Scenario.csv")
+    @Test
     public void paymentFlowShouldBeSuccessful() throws Exception {
 
-        LandingSteps.buyNow();
-        LandingSteps.fillInOrderDetails(amount, username, email, phoneNumber, city, address, postalCode);
-        LandingSteps.submitOrderForm();
+        LandingPage landingSteps = new LandingPage(driver);
+        landingSteps.launchBrowser();
+        landingSteps.buyNow();
+        landingSteps.fillInItemValue(amount);
+        landingSteps.fillInUserDetails(username, email, phoneNumber, city, address, postalCode);
+        landingSteps.submitOrderForm();
 
-        OrderSummarySteps.checkItemName(itemName);
-        OrderSummarySteps.checkItemAmount(amount);
-        OrderSummarySteps.checkShippingName(username);
-        OrderSummarySteps.checkShippingPhoneNumber(phoneNumber);
-        OrderSummarySteps.checkShippingEmail(email);
-        OrderSummarySteps.checkShippingAddress(address);
-        OrderSummarySteps.proceedToPayment();
+        OrderSummaryPage orderSummarySteps = new OrderSummaryPage(driver);
+        orderSummarySteps.checkItemName(itemName);
+        orderSummarySteps.checkItemAmount(amount);
+        orderSummarySteps.checkShippingName(username);
+        orderSummarySteps.checkShippingPhoneNumber(phoneNumber);
+        orderSummarySteps.checkShippingEmail(email);
+        orderSummarySteps.checkShippingAddress(address);
+        orderSummarySteps.proceedToPayment();
 
-        SelectPaymentSteps.selectCreditCardPayment();
+        SelectPaymentPage selectPaymentSteps = new SelectPaymentPage(driver);
+        selectPaymentSteps.selectCreditCardPayment();
 
-        CardDetailsSteps.checkFinalAmount(amount);
-        CardDetailsSteps.inputCardNumber(cardNumberSuccess, expiryDate, cvv);
-        CardDetailsSteps.payNow();
+        CardDetailsPage cardDetailsSteps = new CardDetailsPage(driver);
+        cardDetailsSteps.checkFinalAmount(amount);
+        cardDetailsSteps.inputCardNumber(cardNumberSuccess, expiryDate, cvv);
+        cardDetailsSteps.payNow();
 
-        BankTransactionSteps.typePassword(password);
-        BankTransactionSteps.finaliseTransaction();
+        BankTransactionPage bankTransactionSteps = new BankTransactionPage(driver);
+        bankTransactionSteps.typePassword(password);
+        bankTransactionSteps.finaliseTransaction();
 
-        TransactionStatusSteps.checkTransactionStatus("SUCCESS");
+        TransactionStatusPage transactionStatusSteps = new TransactionStatusPage(driver);
+        transactionStatusSteps.checkTransactionStatus("SUCCESS");
     }
 }
